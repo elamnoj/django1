23a69b7e2714b2578363d8a8d5cfe9d973d6df54
elamnoj <male.jonathan@gmail.com>
2021-07-04 17:25:25 -0500

    added bootstrap

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'JonM',
+        'title': 'Post 1',
+        'content': 'first post content',
+        'date': '07/04/2021'
+    },
+    {
+        'author': 'ElisaS',
+        'title': 'Post 2',
+        'content': 'second post content',
+        'date': '07/05/2021'
+    },
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
-    
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': "About" })
