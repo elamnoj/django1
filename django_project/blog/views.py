fe2dc12bb8079350c4f2e6ba84b294c793678015
elamnoj <male.jonathan@gmail.com>
2021-07-08 20:12:06 -0500

    post styling

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Post
 posts = [
     {
         'author': 'JonM',
@@ -18,7 +17,7 @@ posts = [
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
